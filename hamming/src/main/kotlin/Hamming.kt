240859826833b2365565767c8e5991641a5bf83c
Newton Roncari <new123cesar@gmail.com>
2022-01-04 19:37:25 -0300

    [SOLUTION] - Hamming

---
+++ hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,8 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length." }
+
+        return leftStrand.zip(rightStrand).count { it.first != it.second }
+    }
+}
